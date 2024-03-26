0cb5af8cee15a07ae73d21a092365a55a9953ef9
daosgava <daosgava.garcia@gmail.com>
2024-03-26 17:18:54 +1100

    adding more operations

--- src/index.mjs
+++ src/index.mjs
@@ -1,28 +1,23 @@
-import logger from './logger.mjs';
 import express from 'express';
+import OperationHandler from './operations.mjs';
+import { port } from './constants.mjs';
 
-const app = express();
-const port = 3010;
+const app = express(); 
+const operationHandler = new OperationHandler();
 
-const add = (a, b) => a + b;
-
-app.get('/add', (req, res) => {
-    try {
-        const a = parseInt(req.query.a);
-        const b = parseInt(req.query.b);
-
-        if (isNaN(a) || isNaN(b)) {
-            logger.error('Invalid input');
-            throw new Error('Invalid input');
-        }
-        logger.info('Parameters are valid');
-        res.status(200).send({ data: add(a, b) });
-    } catch (e) {
-        logger.error(e);
-        res.status(500).send(e.message);
-    }
+// Routes
+app.get('/', (_, res) => {
+    res.status(200).send('Calculator API');
 });
 
+app.get('/add', operationHandler.addition);
+
+app.get('/subtract', operationHandler.subtraction);
+
+app.get('/multiply', operationHandler.multiplication);
+
+app.get('/divide', operationHandler.division);
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`🐲: This server at http://localhost:${port}, `);
 });
---
+++ src/operations.mjs
@@ -0,0 +1,83 @@
+import { operations, OPERATION_TYPES } from './constants.mjs';
+import logger from './logger.mjs';
+
+const { ADD, SUBTRACT, MULTIPLY, DIVIDE } = OPERATION_TYPES;
+
+const mathOperations = {
+    [ADD]: (a, b) => a + b,
+    [SUBTRACT]: (a, b) => a - b,
+    [MULTIPLY]: (a, b) => a * b,
+    [DIVIDE]: (a, b) => a / b,
+};
+
+class OperationHandler {
+
+    validateInputs = (inputA, inputB, operationType = ADD) => {
+        const num1 = parseInt(inputA);
+        const num2 = parseInt(inputB);
+
+        if (isNaN(num1)) {
+            throw new Error('a is not a number');
+        }
+
+        if (isNaN(num2)) {
+            throw new Error('b is not a number');
+        }
+
+        if (operationType === DIVIDE && num2 === 0) {
+            throw new Error('division by zero');
+        }
+
+        return [num1, num2];
+    }
+
+    addition = (req, res) => {
+        try {
+            const [num1, num2] = this.validateInputs(req.query.a, req.query.b, ADD);
+            const data = mathOperations[ADD](num1, num2);
+            logger.info({ text: 'valid operation', data: { num1, num2 }, operation: ADD });
+            res.status(200).send({ data });
+        } catch (e) {
+            logger.error({ text: e.message, operation: ADD });
+            res.status(500).send(e.message);
+        }
+    }
+
+    subtraction = (req, res) => {
+        try {
+            const [num1, num2] = this.validateInputs(req.query.a, req.query.b, SUBTRACT);
+            const data = mathOperations[SUBTRACT](num1, num2);
+            logger.info({ text: 'valid operation', data: { num1, num2 }, operation: SUBTRACT });
+            res.status(200).send({ data });
+        } catch (e) {
+            logger.error({ text: e.message, operation: SUBTRACT });
+            res.status(500).send(e.message);
+        }
+    }
+
+    multiplication = (req, res) => {
+        try {
+            const [num1, num2] = this.validateInputs(req.query.a, req.query.b, MULTIPLY);
+            const data = mathOperations[MULTIPLY](num1, num2);
+            logger.info({ text: 'valid operation', data: { num1, num2 }, operation: MULTIPLY });
+            res.status(200).send({ data });
+        } catch (e) {
+            logger.error({ text: e.message, operation: MULTIPLY });
+            res.status(500).send(e.message);
+        }
+    }
+
+    division = (req, res) => {
+        try {
+            const [num1, num2] = this.validateInputs(req.query.a, req.query.b, DIVIDE);
+            const data = mathOperations[DIVIDE](num1, num2);
+            logger.info({ text: 'valid operation', data: { num1, num2 }, operation: DIVIDE });
+            res.status(200).send({ data });
+        } catch (e) {
+            logger.error({ text: e.message, operation: DIVIDE });
+            res.status(500).send(e.message);
+        }
+    }
+}
+
+export default OperationHandler;
